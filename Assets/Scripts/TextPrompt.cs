c232ecf7115087d134bce4966b678db4086260f1
tamanna999saini <tamannas@uoregon.edu>
2023-05-30 23:10:50 -0700

    level2message

---
+++ Assets/Scripts/TextPrompt.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextPrompt : MonoBehaviour
+{
+    public GameObject Text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text.SetActive(false);
+
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Text.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+    }
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(Text);
+    }
+}
